198b422cda29154665d8f51b06ae21a97c285558
Dennis Hedegaard <dennis@dhedegaard.dk>
2015-12-14 20:27:40 +0100

    implemented based on real data, part 2 as well.

--- day14.go
+++ day14.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,6 +17,7 @@ type reindeer struct {
 	location     int
 	currMovesecs int
 	currRestsecs int
+	score        int
 }
 
 func newReindeer(name string, speed int, movesecs int, restsecs int) *reindeer {
@@ -27,48 +32,103 @@ func newReindeer(name string, speed int, movesecs int, restsecs int) *reindeer {
 
 func (r *reindeer) tick() {
 	if r.currMovesecs > 0 {
+		// We're flying.
 		r.currMovesecs--
 		r.location += r.speed
 		if r.currMovesecs == 0 {
+			// We're done flying, start resting.
 			r.currRestsecs = r.restsecs
 		}
 	} else {
+		// We're resting.
 		r.currRestsecs--
 		if r.currRestsecs == 0 {
+			// We're done resting, start flying again.
 			r.currMovesecs = r.movesecs
 		}
 	}
 }
 
 // Race for a given number of ticks (or seconds) with the reindeers given.
-// Returns the reindeer with the highest location.
-func race(reindeers []*reindeer, ticks int) *reindeer {
+// Returns the reindeer with the highest location and score.
+func race(reindeers []*reindeer, ticks int) (*reindeer, *reindeer) {
 	// 3 2 1 race !
 	for i := 0; i < ticks; i++ {
+		// Move all the reindeers one second into the future.
 		for _, r := range reindeers {
 			r.tick()
 		}
-	}
-
-	// Find the reindeer with the maximum location.
-	var result *reindeer
-	for _, r := range reindeers {
-		if result == nil {
-			result = r
-		} else if r.location > result.location {
-			result = r
+		// Find the max locations.
+		var maxLocation int
+		for _, r := range reindeers {
+			if r.location > maxLocation {
+				maxLocation = r.location
+			}
+		}
+		// Give one point to each reindeer at that location.
+		for _, r := range reindeers {
+			if r.location == maxLocation {
+				r.score++
+			}
 		}
 	}
-	return result
+
+	// Find the reindeers with the maximum location/score.
+	var maxDistance, maxScore *reindeer
+	for _, r := range reindeers {
+		if maxDistance == nil && maxScore == nil {
+			maxDistance = r
+			maxScore = r
+			continue
+		}
+
+		if r.location > maxDistance.location {
+			maxDistance = r
+		}
+		if r.score > maxScore.score {
+			maxScore = r
+		}
+	}
+	return maxDistance, maxScore
+}
+
+// Parse input as an interger, or panic().
+func toIntOrPanic(input string) int {
+	output, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+	return output
 }
 
 func main() {
+	// Parse the reindeers into a slice.
+	reInput := regexp.MustCompile(
+		`(\S+?) can fly (\d+?) km/s for (\d+?) seconds, but then must rest for (\d+?) seconds.`)
 	var list []*reindeer
-	list = append(list, newReindeer("comet", 14, 10, 127))
-	list = append(list, newReindeer("dancer", 16, 11, 162))
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		input := scanner.Text()
+		rc := reInput.FindStringSubmatch(input)
+		reindeer := newReindeer(rc[1], toIntOrPanic(rc[2]),
+			toIntOrPanic(rc[3]), toIntOrPanic(rc[4]))
+		list = append(list, reindeer)
+	}
 
+	// Race !
 	before := time.Now()
-	result := race(list, 1000)
-	fmt.Println(result.name, "wins with a distance of", result.location,
-		"took:", time.Now().Sub(before))
+	maxDistance, maxScore := race(list, 2503)
+	fmt.Println(maxDistance.name, "wins part1 with a distance of", maxDistance.location)
+	fmt.Println(maxScore.name, "wins part2 with a score of", maxScore.score)
+	fmt.Println("took:", time.Now().Sub(before))
 }
+
+const input = `Vixen can fly 19 km/s for 7 seconds, but then must rest for 124 seconds.
+Rudolph can fly 3 km/s for 15 seconds, but then must rest for 28 seconds.
+Donner can fly 19 km/s for 9 seconds, but then must rest for 164 seconds.
+Blitzen can fly 19 km/s for 9 seconds, but then must rest for 158 seconds.
+Comet can fly 13 km/s for 7 seconds, but then must rest for 82 seconds.
+Cupid can fly 25 km/s for 6 seconds, but then must rest for 145 seconds.
+Dasher can fly 14 km/s for 3 seconds, but then must rest for 38 seconds.
+Dancer can fly 3 km/s for 16 seconds, but then must rest for 37 seconds.
+Prancer can fly 25 km/s for 6 seconds, but then must rest for 143 seconds.`
