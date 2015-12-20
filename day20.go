2468f9a2b8d7b1b68a5408fefaa752d50f877009
Dennis Hedegaard <dennis@dhedegaard.dk>
2015-12-20 16:20:46 +0100

    Implemented much faster solution, uses a lot more memory.

--- day20.go
+++ day20.go
@@ -6,22 +6,25 @@ import (
 )
 
 func main() {
+	// Calculate number of packages for all possible solutions.
+	fmt.Print("Generating giftcount for houses... ")
 	before := time.Now()
-	for house := 1; true; house++ {
-		result := 0
-		for elf := 1; elf <= house; elf++ {
-			// If the house was visited by the elf, calculate and add to the
-			// score.
-			if house%elf == 0 {
-				result += 10 * (house / elf)
-			}
-		}
-		if house%100 == 0 {
-			fmt.Println("house:", house, "score:", result)
-		}
-		if result >= 33100000 {
-			fmt.Println("First house at or above input:", house, "took:", time.Now().Sub(before))
-			break
+	houses := make(map[int]int)
+	maxIter := goal / 10
+	for house := 1; house < maxIter; house++ {
+		for elf := house; elf < maxIter; elf += house {
+			houses[elf] += house * 10
 		}
 	}
+	fmt.Println("Took", time.Now().Sub(before))
+
+	minKey := -1
+	for key, value := range houses {
+		if key != 1 && value >= goal && (minKey == -1 || key < minKey) {
+			minKey = key
+		}
+	}
+	fmt.Println("Smallest housenumber is:", minKey)
 }
+
+const goal = 33100000
